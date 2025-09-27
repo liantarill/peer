0f26be247ea4e6e8572e31877f3713ed6489ee6e
liantarill <yusufherliananantaril@gmail.com>
2025-09-27 15:12:37 +0700

    menambahkan main

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello world" << endl;
+    cout << "Selamat Datang" << endl;
+    cout << "Selamat Datang" << endl;
+    cout << "Selamat Datang" << endl;
+    cout << "Selamat Datang" << endl;
+    cout << "Selamat Datang" << endl;
+    cout << "Selamat Datang" << endl;
+    cout << "Selamat Datang" << endl;
+    cout << "Selamat Datang" << endl;
+}
