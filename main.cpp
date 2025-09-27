60c7434bec20ace0389ba163936170a8409e1365
liantarill <yusufherliananantaril@gmail.com>
2025-09-27 15:27:03 +0700

    menghapus (coba)

--- main.cpp
+++ main.cpp
@@ -4,14 +4,6 @@ using namespace std;
 int main()
 {
     cout << "Hello world" << endl;
-    cout << "Selamat Datang" << endl;
-    cout << "Selamat Datang" << endl;
-    cout << "Selamat Datang" << endl;
-    cout << "Selamat Datang" << endl;
-    cout << "Selamat Datang" << endl;
-    cout << "Selamat Datang" << endl;
-    cout << "Selamat Datang" << endl;
-    cout << "Selamat Datang" << endl;
 
     cout << "Ini adalah push yang kedua";
 }
